d1f69b3bdcbb63a63ea887fa90927faaf7e044f5
Cora <coratatum@gmail.com>
2022-10-03 16:53:29 -0400

    Initial project setup - add maven, add initial classes

---
+++ rectangles/src/main/java/example/app/Rectangle.java
@@ -0,0 +1,29 @@
+public class Rectangle {
+    private Point bottomLeft;
+    private Point topLeft;
+    private Point topRight;
+    private Point bottomRight;
+
+    public Rectangle(Point bottomLeft, Point topLeft, Point topRight, Point bottomRight) {
+        this.bottomLeft = bottomLeft;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomRight = bottomRight;
+    }
+
+    public Point getBottomLeftPoint() {
+        return this.bottomLeft;
+    }
+
+    public Point getTopLeftPoint() {
+        return this.topLeft;
+    }
+
+    public Point getTopRightPoint() {
+        return this.topRight;
+    }
+
+    public Point getBottomRighPoint() {
+        return this.bottomRight;
+    }
+}
