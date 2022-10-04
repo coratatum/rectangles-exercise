c10b340b39600b3e175f16723ba6956929d90b5a
Cora <coratatum@gmail.com>
2022-10-04 07:44:50 -0400

    add validations

---
+++ rectangles/src/main/java/example/app/RectangleValidationService.java
@@ -0,0 +1,22 @@
+package example.app;
+
+import example.app.errors.InvalidRectangleException;
+
+public class RectangleValidationService {
+    
+    /**
+     * Ensure that a rectangle is valid - that the bottom left corner is below and to the left of the upper right corner
+     * @param rectangle rectangle to validate
+     * @throws InvalidRectangleException if rectangle is not valid
+     */
+    public void validateRectangle(Rectangle rectangle) throws InvalidRectangleException {
+        if (rectangle.getBottomLeftPoint().equals(rectangle.getTopRightPoint())) {
+            throw new InvalidRectangleException("Bottom left point cannot be same as Top Right point");
+        }
+
+        if (rectangle.getBottomLeftPoint().getX() >= rectangle.getTopRightPoint().getX()
+            || rectangle.getBottomLeftPoint().getY() >= rectangle.getTopRightPoint().getY()) {
+                throw new InvalidRectangleException("Bottom Left corner of rectangle must have x and y coordinate of a lower value of Top Right point x and y values.");
+        }
+    }
+}
---
+++ rectangles/src/test/java/example/app/RectangleValidationServiceTest.java
@@ -0,0 +1,33 @@
+package example.app;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import example.app.errors.InvalidRectangleException;
+
+public class RectangleValidationServiceTest {
+    RectangleValidationService service;
+
+    @BeforeClass
+    public void init() {
+        service = new RectangleValidationService();
+    }
+
+    @Test
+    public void testRectangleValidation_isValid() {
+        Rectangle rectangle = new Rectangle(new Point(0,0), new Point(3,4));
+        service.validateRectangle(rectangle);
+    }
+    
+    @Test(expectedExceptions = InvalidRectangleException.class)
+    public void testRectangleValidation_isNotValid_samePoint() {
+        Rectangle rectangle = new Rectangle(new Point(0, 0), new Point(0,0));
+        service.validateRectangle(rectangle);
+    }
+
+    @Test(expectedExceptions = InvalidRectangleException.class)
+    public void testRectangleValidation_isNotValid_incorrectOrder() {
+        Rectangle rectangle = new Rectangle(new Point(5, 8), new Point(0,0));
+        service.validateRectangle(rectangle);
+    }
+}
