0efb6398c4f711833949cd2b8c78b91ddecc2ddb
Cora <coratatum@gmail.com>
2022-10-04 10:02:42 -0400

    Start adding spring boot endpoints because I've lost control of my life

---
+++ rectangles/src/main/java/example/app/RectanglesApplication.java
@@ -0,0 +1,12 @@
+package example.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RectanglesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RectanglesApplication.class, args);
+  }
+
+}
